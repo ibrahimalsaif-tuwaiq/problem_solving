f0133b9cfffb6b14a03b4ff6151364b5f844df2c
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-08 10:52:56 +0300

    function isPalindrome(string) {

---
+++ isPalindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(string) {
+  const newString = string.split("").reverse().join("").toLowerCase();
+  if (string.toLowerCase() === newString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("bob")); //true
+console.log(isPalindrome("hello")); //false
